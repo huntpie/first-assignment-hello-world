e1ae4b55c35a8f1d12ac163fad6386b2158596df
Pierce Hunt <huntpie@gmail.com>
2020-05-12 09:58:29 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace first_assignment_hello_world
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
